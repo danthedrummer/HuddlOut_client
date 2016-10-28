7caad10e32b3143a886c30f645277ea2b9567487
Reccy <aaronmeaney@hotmail.com>
2016-10-28 11:02:22 +0100

    Added REST call example.

--- app/src/main/java/com/teamh/huddleout/MainActivity.java
+++ app/src/main/java/com/teamh/huddleout/MainActivity.java
@@ -6,11 +6,16 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.android.volley.*;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
 public class MainActivity extends Activity {
 
     //Hello World activity - testing styles
 
-    private enum Theme { DeepPurple, Purple, Lime, LightGreen }
+    private enum Theme {DeepPurple, Purple, Lime, LightGreen}
+
     static private Theme currentTheme = Theme.DeepPurple;
 
     //Add buttons to activity
@@ -19,12 +24,15 @@ public class MainActivity extends Activity {
     Button buttonLime;
     Button buttonLightGreen;
 
+    //Add result text view
+    TextView resultView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         //Apply current theme
-        switch(currentTheme) {
+        switch (currentTheme) {
             case DeepPurple:
                 setTheme(R.style.DeepPurpleTheme);
                 break;
@@ -41,11 +49,14 @@ public class MainActivity extends Activity {
 
         setContentView(R.layout.activity_main);
 
-        //Set element instances
-        buttonDeepPurple = (Button) findViewById(R.id.buttonDeepPurple);
-        buttonPurple = (Button) findViewById(R.id.buttonPurple);
-        buttonLime = (Button) findViewById(R.id.buttonLime);
-        buttonLightGreen = (Button) findViewById(R.id.buttonLightGreen);
+        //Add buttons to activity
+        buttonDeepPurple = (Button)findViewById(R.id.buttonDeepPurple);
+        buttonPurple = (Button)findViewById(R.id.buttonPurple);
+        buttonLime = (Button)findViewById(R.id.buttonLime);
+        buttonLightGreen = (Button)findViewById(R.id.buttonLightGreen);
+
+        //Add result text view
+        resultView = (TextView)findViewById(R.id.resultView);
 
         //Add action listeners for buttons
         buttonDeepPurple.setOnClickListener(new View.OnClickListener() {
@@ -71,6 +82,27 @@ public class MainActivity extends Activity {
                 changeTheme(Theme.LightGreen);
             }
         });
+
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "https://huddlout-server-reccy.c9users.io:8081/getTime";
+
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        resultView.setText("Response is: " + response);
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                resultView.setText("That didn't work!");
+            }
+        });
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
     }
 
     //Changes the theme (Prototype - Needs actual StyleManager.java class!)
