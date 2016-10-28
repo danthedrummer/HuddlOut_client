e5cebce96235893706ac6c895b58bda22a40f3a7
Dan Downey <danthedrummerrof@gmail.com>
2016-10-28 15:06:07 +0100

    Login screen swapping to main screen on button press

--- app/src/main/java/com/teamh/huddleout/LoginActivity.java
+++ app/src/main/java/com/teamh/huddleout/LoginActivity.java
@@ -2,6 +2,9 @@ package com.teamh.huddleout;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
+import android.content.Intent;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button loginButton = (Button)findViewById(R.id.loginButton);
+
+        loginButton.setOnClickListener(
+                new Button.OnClickListener(){
+                    public void onClick(View v){
+                        startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                    }
+                }
+        );
     }
 }
