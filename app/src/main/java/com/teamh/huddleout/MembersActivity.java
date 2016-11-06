ab78dc7e0f10a86b15fb35c1c00cc7fdc9719a3f
Reccy <aaronmeaney@hotmail.com>
2016-11-06 13:32:16 +0000

    Added profile page for other users in group members section.

--- app/src/main/java/com/teamh/huddleout/MembersActivity.java
+++ app/src/main/java/com/teamh/huddleout/MembersActivity.java
@@ -2,6 +2,8 @@ package com.teamh.huddleout;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MembersActivity extends AppCompatActivity {
 
@@ -10,5 +12,42 @@ public class MembersActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_members);
         this.setTitle("Group Members");
+
+        ImageButton memberButton1 = (ImageButton)findViewById(R.id.memberButton1);
+        ImageButton memberButton2 = (ImageButton)findViewById(R.id.memberButton2);
+        ImageButton memberButton3 = (ImageButton)findViewById(R.id.memberButton3);
+        ImageButton memberButton4 = (ImageButton)findViewById(R.id.memberButton4);
+
+        memberButton1.setOnClickListener(
+            new ImageButton.OnClickListener(){
+                public void onClick(View v){
+                    ActivitySwap.swapToNextActivity(MembersActivity.this, OtherProfileActivity.class);
+                }
+            }
+        );
+
+        memberButton2.setOnClickListener(
+                new ImageButton.OnClickListener(){
+                    public void onClick(View v){
+                        ActivitySwap.swapToNextActivity(MembersActivity.this, OtherProfileActivity.class);
+                    }
+                }
+        );
+
+        memberButton3.setOnClickListener(
+                new ImageButton.OnClickListener(){
+                    public void onClick(View v){
+                        ActivitySwap.swapToNextActivity(MembersActivity.this, OtherProfileActivity.class);
+                    }
+                }
+        );
+
+        memberButton4.setOnClickListener(
+                new ImageButton.OnClickListener(){
+                    public void onClick(View v){
+                        ActivitySwap.swapToNextActivity(MembersActivity.this, OtherProfileActivity.class);
+                    }
+                }
+        );
     }
 }
