1db532883f342232736c99d279a977acf33a3ab2
Reccy <aaronmeaney@hotmail.com>
2016-11-06 17:48:20 +0000

    Added Chat to group menu.

--- app/src/main/java/com/teamh/huddleout/ChatFragment.java
+++ app/src/main/java/com/teamh/huddleout/ChatFragment.java
@@ -1,12 +1,21 @@
 package com.teamh.huddleout;
 
 import android.content.Context;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
 
 
 /**
@@ -18,14 +27,6 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class ChatFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     private OnFragmentInteractionListener mListener;
 
@@ -33,14 +34,11 @@ public class ChatFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ChatFragment.
-     */
+    LinearLayout chatLayout;
+    ImageButton sendButton;
+    EditText msgInput;
+    ScrollView scroll;
+
     // TODO: Rename and change types and number of parameters
     public static ChatFragment newInstance() {
         ChatFragment fragment = new ChatFragment();
@@ -52,17 +50,27 @@ public class ChatFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_chat, container, false);
+        View v = inflater.inflate(R.layout.fragment_chat, container, false);
+
+        chatLayout = (LinearLayout)v.findViewById(R.id.chatLayout);
+        sendButton = (ImageButton)v.findViewById(R.id.sendButton);
+        msgInput = (EditText)v.findViewById(R.id.messageTextInput);
+        scroll = (ScrollView)v.findViewById(R.id.chatScrollView);
+
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addBubble(msgInput.getText().toString());
+            }
+        });
+
+        return v;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -87,6 +95,40 @@ public class ChatFragment extends Fragment {
     public void onDetach() {
         super.onDetach();
         mListener = null;
+    }
+
+    private void addBubble(String msg) {
+
+        //Set text params
+        msgInput.setText("");
+        RelativeLayout rl = new RelativeLayout(getContext());
+        TextView text = new TextView(getContext());
+        text.setText(msg);
+        text.setTextColor(Color.parseColor("#ffffff"));
+        text.setBackgroundColor(Color.parseColor("#03a9f4"));
+        text.setPadding(5, 5, 5, 5);
+
+        //Add text
+        rl.addView(text);
+
+        //Set layout params
+        rl.setGravity(Gravity.RIGHT);
+        LinearLayout.LayoutParams relativeParams = new LinearLayout.LayoutParams(
+                new LinearLayout.LayoutParams(
+                        LinearLayout.LayoutParams.MATCH_PARENT,
+                        LinearLayout.LayoutParams.WRAP_CONTENT));
+        relativeParams.setMargins(5, 5, 5, 5);
+        rl.setLayoutParams(relativeParams);
+        rl.requestLayout();
+        chatLayout.addView(rl);
+
+        //Scroll down
+        scroll.post(new Runnable() {
+            @Override
+            public void run() {
+                scroll.fullScroll(View.FOCUS_DOWN);
+            }
+        });
     }
 
     /**
