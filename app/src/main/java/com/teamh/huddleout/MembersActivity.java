e3513542442bd1c737196b5e399c68aac33ced2b
Reccy <aaronmeaney@hotmail.com>
2016-12-12 14:09:34 +0000

    Finished layout for group members fragment.

--- app/src/main/java/com/teamh/huddleout/MembersActivity.java
+++ app/src/main/java/com/teamh/huddleout/MembersActivity.java
@@ -12,42 +12,5 @@ public class MembersActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_members);
         this.setTitle("Group Members");
-
-        ImageButton memberButton1 = (ImageButton)findViewById(R.id.memberButton1);
-        ImageButton memberButton2 = (ImageButton)findViewById(R.id.memberButton2);
-        ImageButton memberButton3 = (ImageButton)findViewById(R.id.memberButton3);
-        ImageButton memberButton4 = (ImageButton)findViewById(R.id.memberButton4);
-
-        memberButton1.setOnClickListener(
-            new ImageButton.OnClickListener(){
-                public void onClick(View v){
-                    ActivitySwap.swapToNextActivity(MembersActivity.this, OtherProfileActivity.class);
-                }
-            }
-        );
-
-        memberButton2.setOnClickListener(
-                new ImageButton.OnClickListener(){
-                    public void onClick(View v){
-                        ActivitySwap.swapToNextActivity(MembersActivity.this, OtherProfileActivity.class);
-                    }
-                }
-        );
-
-        memberButton3.setOnClickListener(
-                new ImageButton.OnClickListener(){
-                    public void onClick(View v){
-                        ActivitySwap.swapToNextActivity(MembersActivity.this, OtherProfileActivity.class);
-                    }
-                }
-        );
-
-        memberButton4.setOnClickListener(
-                new ImageButton.OnClickListener(){
-                    public void onClick(View v){
-                        ActivitySwap.swapToNextActivity(MembersActivity.this, OtherProfileActivity.class);
-                    }
-                }
-        );
     }
 }
